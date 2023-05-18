410d4f50bedb40ba04cafedfd7c5f971b23f440c
Ryan Morgan <R.Morgan5399@gmail.com>
2023-05-18 16:05:40 -0700

    Create project outline

--- assignment2/src/Main.java
+++ assignment2/src/Main.java
@@ -1,17 +1,70 @@
 public class Main {
 
+    public static void main(String[] args) {
+
+        System.out.println(getArmstrongNum());
+    }
+
     /**
-     * Write a program to print whether a number is even or odd, also take input from the user.
-     * Take name as input and print a greeting message for that particular name.
-     * Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
-     * Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
-     * Take 2 numbers as input and print the largest number.
-     * Input currency in rupees and output in USD.
-     * To calculate Fibonacci Series up to n numbers.
-     * To find out whether the given String is Palindrome or not.
      * To find Armstrong Number between two given number.
      */
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static int getArmstrongNum(int a, int b) {
+        return 7;
+    }
+
+    /**
+     * To find out whether the given String is Palindrome or not.
+     */
+    private static boolean isPalindrome(String a, String b) {
+        return true;
+    }
+
+    /**
+     * To calculate Fibonacci Series up to n numbers.
+     */
+    private static int getFibonacci(int n) {
+        return 0;
+    }
+
+    /**
+     * Input currency in rupees and output in USD.
+     */
+    private static double convertCurrency(double rupees) {
+        return 77.7;
+    }
+
+    /**
+     * Take 2 numbers as input and print the largest number.
+     */
+    private static double compareNums() {
+        return 7;
+    }
+
+    /**
+     * Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+     */
+    private static double calculate() {
+        return 33.33;
+    }
+
+    /**
+     * Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+     */
+    private static double calculateInterest(double principal, int time, float rate) {
+        return 0;
+    }
+
+    /**
+     * Take name as input and print a greeting message for that particular name.
+     */
+    private static void printGreeting(String name) {
+        return;
+    }
+
+    /**
+     * Write a program to print whether a number is even or odd, also take input from the user.
+     */
+    private static boolean isEven(int num) {
+        return true;
     }
 }
